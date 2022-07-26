db6d55ac6fc40a0709acddb8f740495c18d107f0
Eneier <canadacoder94@gmail.com>
2022-07-26 12:10:44 -0400

    PostIdPage changes and Hook

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,22 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import {useFetching} from "../hooks/useFetching";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/Loader/Loader";
 
 const PostIdPage = () => {
-    const {id} = useParams()
+
+    const params = useParams()
     const [post, setPost] = useState({})
 
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(params.id)
+        setPost(response.data)
+    })
+
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then(res => res.json())
-            .then(data => setPost(data))
-    }, [id])
+        fetchPostById(params.id)
+    }, [])
     return (
         <div>
-            <h1>The page ID is {id}</h1>
-            <div>
-               <h1>{post.title}</h1>
-                <p>{post.body}</p>
-            </div>
+            <h1>The page ID is {params.id}</h1>
+            {isLoading
+                ? <Loader/>
+                : <div>
+                    <ul>
+                        <li>{post.title}</li>
+                        <li>{post.body}</li>
+                    </ul>
+                </div>
+            }
         </div>
     );
 };
